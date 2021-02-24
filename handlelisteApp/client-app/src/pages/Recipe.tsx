06a527ea445593ab624183e8dbba0b39bc361108
Oddmund Huseby <huseby.oddmund@gmail.com>
2021-02-24 13:25:40 +0100

    Renamed some pages and deleted files

---
+++ handlelisteApp/client-app/src/pages/Recipe.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export const Recipe: React.FC<Props> = () => {
+  return <div></div>;
+};
--- handlelisteApp/client-app/src/pages/ShoppingLists.tsx
+++ handlelisteApp/client-app/src/pages/ShoppingLists.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 interface Props {}
 
-export const Stock: React.FC<Props> = () => {
+export const ShoppingLists: React.FC<Props> = () => {
   return <div></div>;
 };
