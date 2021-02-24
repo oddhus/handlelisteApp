b480865781ab8549c0614fc57af4f1ca21cd7de8
Oddmund Huseby <huseby.oddmund@gmail.com>
2021-02-24 13:25:00 +0100

    added route component

---
+++ handlelisteApp/client-app/src/routes/AppRoutes.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { CreateRecipe } from "../pages/CreateRecipe";
+import { Recipes } from "../pages/Recipes";
+import { SignIn } from "../pages/SignIn";
+import { SignUp } from "../pages/SignUp";
+import { Recipe } from "../pages/Recipe";
+import { ShoppingLists } from "../pages/ShoppingLists";
+import { ShoppingList } from "../pages/ShoppingList";
+import { Settings } from "../pages/Settings";
+import { Household } from "../pages/Household";
+
+interface Props {}
+
+export const AppRoutes: React.FC<Props> = () => {
+  return (
+    <React.Fragment>
+      <Switch>
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/create-recipe" component={CreateRecipe} />
+        <Route path="/recipes" component={Recipes} />
+        <Route path="/recipes/:recipeId" component={Recipe} />
+        <Route path="/shopping-list" component={ShoppingLists} />
+        <Route path="/shopping-list/:listId" component={ShoppingList} />
+        <Route path="/settings" component={Settings} />
+        <Route path="/household" component={Household} />
+      </Switch>
+    </React.Fragment>
+  );
+};
---
+++ handlelisteApp/client-app/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export const ProtectedRoute: React.FC<Props> = () => {
+  return <div></div>;
+};
