dea7188acf4da54165e11ec837de3c466010909c
Kristoffer <31278784+krnome@users.noreply.github.com>
2021-02-24 14:07:34 +0100

    Initial database: User, Item, ItemOnShoppingList, ShoppingList

---
+++ handlelisteApp/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace handlelisteApp.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+        public String ItemName { get; set; }
+        public double Weight { get; set; }
+
+    }
+}
