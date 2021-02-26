fab619d3c655890c259e9b662b845f1f75ef58f9
RogerWisnes <rogerwisnes@gmail.com>
2021-02-26 18:37:34 +0100

    Expanded the english language

--- handlelisteApp/client-app/src/lang/en.tsx
+++ handlelisteApp/client-app/src/lang/en.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 
 export const
-    shoppingList = (): string => "Shopping list",
-    createShoppingList = (): string => "Create shopping list", 
+    homePage = (): string => "HomePage",
+
+    signUp = (): string => "Sign up",
+
     login = (): string => "Login",
     emailAddress = (): string => "Email address",
     password = (): string => "Password",
-    weNeverShareEmail = (): string => "We will never share your email."
+    weNeverShareEmail = (): string => "We will never share your email.",
+
+    shoppingList = (): string => "Shopping list",
+    shoppingLists = (): string => "Shopping lists",
+    createShoppingList = (): string => "Create shopping list",
+
+    recipes = (): string => "Recipes",
+    recipe = (): string => "Recipe",
+    createRecipe = (): string => "Create recipe",
+
+    settings = (): string => "Settings",
+
+    household = (): string => "Household",
+    
+    noMatch = (): string => "No match"
+    
