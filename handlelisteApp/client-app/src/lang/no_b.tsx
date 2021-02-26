e71b060d7f3d7356bfd4dbf1ddbca0ca4737f634
RogerWisnes <rogerwisnes@gmail.com>
2021-02-26 18:52:30 +0100

    Updated norwegian language to match english

--- handlelisteApp/client-app/src/lang/no_b.tsx
+++ handlelisteApp/client-app/src/lang/no_b.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 
 export const
-    sayHello = (): string => "Hei",
-    shoppingList = (): string => "Handleliste",
-    createShoppingList = (): string => "Lag handleliste", 
+    homePage = (): string => "Hjemmeside",
+
+    signUp = (): string => "Registrer ny bruker",
+
     login = (): string => "Logg inn",
     emailAddress = (): string => "Epost addresse",
     password = (): string => "Passord",
-    weNeverShareEmail = (): string => "Vi vil ikke dele din epost addresse med noen."
+    weNeverShareEmail = (): string => "Vi vil ikke dele din epost addresse med noen.",
+
+    shoppingList = (): string => "Handleliste",
+    shoppingLists = (): string => "Handlelister",
+    createShoppingList = (): string => "Lag ny handleliste",
+
+    recipes = (): string => "Oppskrifter",
+    recipe = (): string => "Oppskrift",
+    createRecipe = (): string => "Lag ny oppskrift",
+
+    settings = (): string => "Innstillinger",
+
+    household = (): string => "Hushold",
+
+    noMatch = (): string => "Ingen treff"
