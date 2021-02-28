1a037a1bcb80c48b6d3b1b5413e75f02f5768b33
Severinzz <36744838+Severinzz@users.noreply.github.com>
2021-02-28 19:53:16 +0100

    Minor cleanup in shoppinglist
    Minor cleanup
    Added props to shoppinglist
    Fixed bug with headers

--- handlelisteApp/client-app/src/components/shoppingList/shoppingList.tsx
+++ handlelisteApp/client-app/src/components/shoppingList/shoppingList.tsx
@@ -10,44 +10,14 @@ import {
   Checkbox,
 } from '@chakra-ui/react';
 import { ArrowDownIcon } from '@chakra-ui/icons'
+import {Iitem} from "../../models/ShoppingList"
 
-interface Props {}
 
-interface Iitem {
-    category: string,
-    item: string,
-    qunatity: number,
-    unit: string
+interface Props {
+  items: Iitem[]
 }
 
-var item: Iitem = {
-  category: 'meieri',
-  item: 'egg',
-  qunatity: 1,
-  unit: 'stk',
-};
 
-var item1: Iitem = {
-  category: 'Fryse',
-  item: 'laks',
-  qunatity: 3,
-  unit: 'stk',
-};
-
-var item2: Iitem = {
-  category: 'Baking',
-  item: 'mjøl',
-  qunatity: 2,
-  unit: 'kg',
-};
-
-var item3: Iitem = {
-  category: 'meieri',
-  item: 'melk',
-  qunatity: 3,
-  unit: 'liter',
-};
-var dummyData: Iitem[] = [item, item1, item2, item3];
 
 function setupTableBody(itemsList: Iitem[]) {
   return itemsList.map((items) => (
@@ -75,7 +45,7 @@ function getListOfCategories(itemsList: { category: string }[]) {
 function setupTables(itemsList: Iitem[]){
     var categories: string[] = getListOfCategories(itemsList)
     var tables: React.ReactFragment[] = []
-    var strictHeaders = Language.shoppingLists();
+    var strictHeaders = Language.shoppingList();
     categories.map(category => {
         let categorizedItems: Iitem[] = []
         itemsList.map(item => {
@@ -107,8 +77,8 @@ function setupTables(itemsList: Iitem[]){
     return tables
     }
 
-export const ListComponent: React.FC<Props> = () => {
-  var lists = setupTables(dummyData);
+export const ListComponent: React.FC<Props> = ({items}) => {
+  var lists = setupTables(items);
 
   return (
     <div>
