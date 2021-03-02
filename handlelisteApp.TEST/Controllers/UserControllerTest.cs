7378ef84354f85e46f654ccbf17fa78159c60b6b
Kristoffer <31278784+krnome@users.noreply.github.com>
2021-03-02 16:56:08 +0100

    UserController/Service: Moved hashing of password to service, Commented out [Authorize] used for testing; UserControllerTest: Started implementing tests

---
+++ handlelisteApp.TEST/Controllers/UserControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using handlelisteApp.Context;
+using handlelisteApp.Controllers;
+using handlelisteApp.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace handlelisteApp.Test.Controllers
+{
+    public class UserControllerTest
+    {
+        private Mock<IUserService> _mockService;
+        private Mock<ShoppingListContext> _mockContext;
+        private UserController _controller;
+
+        [Fact]
+        public void ControllerShouldCallServiceWhenCreatingNewUser()
+        {
+
+        }
+
+    }
+}
