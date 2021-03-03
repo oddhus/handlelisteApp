444f30439fdf09672c2f42381d6b9f7a4ff8fd74
RogerWisnes <rogerwisnes@gmail.com>
2021-03-03 12:16:11 +0100

    Imported radio-button-value from settings

--- handlelisteApp/client-app/src/lang/ActiveLanguage.tsx
+++ handlelisteApp/client-app/src/lang/ActiveLanguage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import * as no_b from './no_b'
 import * as en from './en'
+import * as settings from '../pages/Settings'
 
-export const Language = en
+export const Language =  en //settings.radioCheckedLanguage()
 
