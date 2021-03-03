fc78b84413ea0d3fbcc5300b2311f9f6deb3bc88
RogerWisnes <rogerwisnes@gmail.com>
2021-03-03 12:14:26 +0100

    Added missing settings-route

--- handlelisteApp/client-app/src/routes/AppRoutes.tsx
+++ handlelisteApp/client-app/src/routes/AppRoutes.tsx
@@ -26,6 +26,7 @@ export const AppRoutes: React.FC<Props> = () => {
             <Route exact path="/recipes/:recipeId" component={Recipe}/>
             <Route exact path="/shopping-list" component={ShoppingLists}/>
             <Route exact path="/shopping-list/:listId" component={ShoppingList}/>
+            <Route exact path="/settings" component={Settings}/>
             <Route exact path="/settings/user" component={Settings}/>
             <Route exact path="/settings/accessibility" component={Settings}/>
             <Route exact path="/settings/blacklist" component={Settings}/>
