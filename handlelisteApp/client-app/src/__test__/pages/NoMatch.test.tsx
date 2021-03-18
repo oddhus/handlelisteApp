a1fedbb67c60c940ed6bb8754595f163354260d8
orjanen <superszura@gmail.com>
2021-03-18 15:12:09 +0100

    fixed test

--- handlelisteApp/client-app/src/__test__/pages/NoMatch.test.tsx
+++ handlelisteApp/client-app/src/__test__/pages/NoMatch.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import { NoMatch } from '../../pages/NoMatch'
+import { NoMatch } from '../../pages/errors/NoMatch'
 import English from '../../lang/en'
 import { MockLanguage } from '../MockLanguage'
 
