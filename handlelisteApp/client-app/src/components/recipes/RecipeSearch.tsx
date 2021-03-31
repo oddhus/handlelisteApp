1726947493eb24ff1af8c39ff1c132c575d758bb
Severinzz <36744838+Severinzz@users.noreply.github.com>
2021-03-31 14:02:21 +0200

    Fix stupid fucking misstake.
    Now uses useStore instead of store direclty....

--- handlelisteApp/client-app/src/components/recipes/RecipeSearch.tsx
+++ handlelisteApp/client-app/src/components/recipes/RecipeSearch.tsx
@@ -2,12 +2,13 @@ import { Search2Icon } from '@chakra-ui/icons'
 import { InputGroup, InputLeftElement, Input } from '@chakra-ui/react'
 import { observer } from 'mobx-react-lite'
 import React from 'react'
-import { store } from '../../stores/store'
+import { useStore } from '../../stores/store'
 import { RecipeList } from './RecipeList'
 
 interface Props {}
 
 export const RecipeSearch: React.FC<Props> = observer(() => {
+  const { settingStore, recipeStore } = useStore() 
   return (
     <div>
       <InputGroup>
@@ -16,9 +17,9 @@ export const RecipeSearch: React.FC<Props> = observer(() => {
           children={<Search2Icon color="gray.300" />}
         />
         <Input 
-        onChange={(e) => store.recipeStore.searchInRecipies(e.target.value.toLowerCase())}
+        onChange={(e) => recipeStore.searchInRecipies(e.target.value.toLowerCase())}
         type="tel" 
-        placeholder={store.settingStore.language.search} />
+        placeholder={settingStore.language.search} />
       </InputGroup>
       <RecipeList editable={false} deleteable={false} />
     </div>
