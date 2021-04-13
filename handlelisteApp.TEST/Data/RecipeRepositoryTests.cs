3869505cb017e1928229f6a9872e203cd646ab34
Kristoffer <31278784+krnome@users.noreply.github.com>
2021-04-13 21:06:44 +0200

    More tests on RecipeRepository

--- handlelisteApp.TEST/Data/RecipeRepositoryTests.cs
+++ handlelisteApp.TEST/Data/RecipeRepositoryTests.cs
@@ -14,7 +14,8 @@ namespace handlelisteApp.Test.Data
 {
     public class RecipeRepositoryTests
     {
-        private Mock<DbSet<Recipe>> _mockSet;
+        private Mock<DbSet<Recipe>> _mockRecipeSet;
+        private Mock<DbSet<SavedRecipe>> _mockSavedRecipeSet;
         private Mock<ShoppingListContext> _mockContext;
 
         private RecipeRepository _recipeRepository;
@@ -35,6 +36,9 @@ namespace handlelisteApp.Test.Data
         private Recipe MilkRecipe2;
         private Recipe CheeseRecipe;
         private Recipe MilkAndEggsRecipe;
+
+        private SavedRecipe savedRecipe1;
+        private SavedRecipe savedRecipe2;
 
         public RecipeRepositoryTests()
         {
@@ -91,23 +95,40 @@ namespace handlelisteApp.Test.Data
                 UserID = 2
             };
 
-            var data = new List<Recipe>
+            var recipeData = new List<Recipe>
             {
                 EggRecipe, EggRecipe2, MilkRecipe, MilkRecipe2, CheeseRecipe, MilkAndEggsRecipe
 
 
             }.AsQueryable();
 
+            
 
 
-            _mockSet = new Mock<DbSet<Recipe>>();
-            _mockSet.As<IQueryable<Recipe>>().Setup(m => m.Provider).Returns(data.Provider);
-            _mockSet.As<IQueryable<Recipe>>().Setup(m => m.Expression).Returns(data.Expression);
-            _mockSet.As<IQueryable<Recipe>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            _mockSet.As<IQueryable<Recipe>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            _mockRecipeSet = new Mock<DbSet<Recipe>>();
+            _mockRecipeSet.As<IQueryable<Recipe>>().Setup(m => m.Provider).Returns(recipeData.Provider);
+            _mockRecipeSet.As<IQueryable<Recipe>>().Setup(m => m.Expression).Returns(recipeData.Expression);
+            _mockRecipeSet.As<IQueryable<Recipe>>().Setup(m => m.ElementType).Returns(recipeData.ElementType);
+            _mockRecipeSet.As<IQueryable<Recipe>>().Setup(m => m.GetEnumerator()).Returns(recipeData.GetEnumerator());
+
+
+            savedRecipe1 = new SavedRecipe { RecipeId = MilkRecipe.RecipeID, Recipe = MilkRecipe, UserId = 1 };
+            savedRecipe2 = new SavedRecipe { RecipeId = EggRecipe.RecipeID, Recipe = EggRecipe, UserId = 1 };
+
+            var savedRecipeData = new List<SavedRecipe> { savedRecipe1, savedRecipe2 }.AsQueryable();
+
+            _mockSavedRecipeSet = new Mock<DbSet<SavedRecipe>>();
+            _mockSavedRecipeSet.As<IQueryable<SavedRecipe>>().Setup(m => m.Provider).Returns(savedRecipeData.Provider);
+            _mockSavedRecipeSet.As<IQueryable<SavedRecipe>>().Setup(m => m.Expression).Returns(savedRecipeData.Expression);
+            _mockSavedRecipeSet.As<IQueryable<SavedRecipe>>().Setup(m => m.ElementType).Returns(savedRecipeData.ElementType);
+            _mockSavedRecipeSet.As<IQueryable<SavedRecipe>>().Setup(m => m.GetEnumerator()).Returns(savedRecipeData.GetEnumerator());
+
+
 
             _mockContext = new Mock<ShoppingListContext>();
-            _mockContext.Setup(m => m.Recipes).Returns(_mockSet.Object);
+            _mockContext.Setup(m => m.Recipes).Returns(_mockRecipeSet.Object);
+            _mockContext.Setup(m => m.SavedRecipes).Returns(_mockSavedRecipeSet.Object);
 
             _recipeRepository = new RecipeRepository(_mockContext.Object);
 
@@ -180,6 +201,97 @@ namespace handlelisteApp.Test.Data
             Assert.DoesNotContain(EggRecipe, result);
             Assert.DoesNotContain(EggRecipe2, result);
             Assert.DoesNotContain(CheeseRecipe, result);
+        }
+
+        [Fact]
+        public void GetAllRecipesUsingSeveralItemsReturnsRecipesUsingThoseItemsAndNoOtherRecipes()
+        {
+            List<Item> items = new List<Item> { Milk, Eggs };
+            var result = _recipeRepository.GetAllRecipesUsingSeveralItems(items);
+
+            Assert.Contains(MilkRecipe, result);
+            Assert.Contains(MilkRecipe2, result);
+            Assert.Contains(EggRecipe, result);
+            Assert.Contains(EggRecipe2, result);
+            Assert.Contains(MilkAndEggsRecipe, result);
+            Assert.DoesNotContain(CheeseRecipe, result);
+        }
+
+        [Fact]
+        public void UpdatingANullItemShouldThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(() => _recipeRepository.UpdateRecipe(0, null));
+        }
+
+        [Fact]
+        public void UpdatingAnItemShouldCallSaveChangesOnContext()
+        {
+            Recipe updatedRecipe = MilkRecipe;
+            updatedRecipe.Approach = "Updated approach";
+            _recipeRepository.UpdateRecipe(1, updatedRecipe);
+
+            _mockContext.Verify(m => m.SaveChanges(), Times.Once);
+        }
+
+        [Fact]
+        public void UpdatingAnItemShouldCallUpdateOnContext()
+        {
+            Recipe updatedRecipe = MilkRecipe;
+            updatedRecipe.Approach = "Updated approach";
+            _recipeRepository.UpdateRecipe(1, updatedRecipe);
+
+            _mockContext.Verify(m => m.Recipes.Update(It.IsAny<Recipe>()), Times.Once);
+
+        }
+
+        [Fact]
+        public void GetSavedRecipesShouldReturnOnlySavedRecipesAndNoOtherRecipes()
+        {
+            var result = _recipeRepository.GetSavedRecipes(1);
+
+            Assert.Contains(MilkRecipe, result);
+            Assert.Contains(EggRecipe, result);
+            Assert.DoesNotContain(MilkRecipe2, result);
+            Assert.DoesNotContain(EggRecipe2, result);
+            Assert.DoesNotContain(MilkAndEggsRecipe, result);
+            Assert.DoesNotContain(CheeseRecipe, result);
+        }
+
+        [Fact]
+        public void SaveRecipeShouldCallSaveChangesOnContext()
+        {
+            SavedRecipe savedRecipe = new SavedRecipe { Recipe = CheeseRecipe, UserId = 1 };
+
+            _recipeRepository.SaveRecipe(savedRecipe);
+
+            _mockContext.Verify(m => m.SaveChanges(), Times.Once);
+
+        }
+
+        [Fact]
+        public void SaveRecipeShouldCallAddOnContext()
+        {
+            SavedRecipe savedRecipe = new SavedRecipe { Recipe = CheeseRecipe, UserId = 1 };
+
+            _recipeRepository.SaveRecipe(savedRecipe);
+
+            _mockContext.Verify(m => m.SavedRecipes.Add(It.IsAny<SavedRecipe>()), Times.Once);
+        }
+
+        [Fact]
+        public void DeleteSavedRecipeShouldCallRemoveOnContext()
+        {
+            _recipeRepository.DeleteSavedRecipe(savedRecipe1);
+
+            _mockContext.Verify(m => m.SavedRecipes.Remove(It.IsAny<SavedRecipe>()), Times.Once);
+        }
+
+        [Fact]
+        public void DeleteSavedRecipeShouldCallSaveChangesOnContext()
+        {
+            _recipeRepository.DeleteSavedRecipe(savedRecipe1);
+
+            _mockContext.Verify(m => m.SaveChanges(), Times.Once);
         }
 
     }
