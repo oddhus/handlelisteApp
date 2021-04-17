a50fb2ef9ba0d2a1a76b661b08c27112215d82da
Severinzz <36744838+Severinzz@users.noreply.github.com>
2021-04-18 00:38:53 +0200

    Remove duplicate cypress config

--- handlelisteApp/client-app/cypress/support/commands.js
+++ handlelisteApp/client-app/cypress/support/commands.js
@@ -23,7 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.config('pageLoadTimeout', 180000)
 Cypress.Commands.add('login', (username, password) => {
   console.log(username)
   console.log(password)
