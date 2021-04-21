9e1161be65bbc612b9eec38691f5676ca689293c
Kristoffer <31278784+krnome@users.noreply.github.com>
2021-04-21 13:58:06 +0200

    Added [ExcludeFromCodeCoverage] to Model classes and Pages/Error

--- handlelisteApp/Models/MyKitchen.cs
+++ handlelisteApp/Models/MyKitchen.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace handlelisteApp.Models
 {
+    [ExcludeFromCodeCoverage]
     public class MyKitchen
     {
         public int MyKitchenID { get; set; }
--- handlelisteApp/Models/ShoppingList.cs
+++ handlelisteApp/Models/ShoppingList.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace handlelisteApp.Models
 {
+    [ExcludeFromCodeCoverage]
     public class ShoppingList
     {
         public int ShoppingListID { get; set; }
